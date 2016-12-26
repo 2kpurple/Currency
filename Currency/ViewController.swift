d1ba7ebcece9d128886ecd6a6e6c82815c2afb22
2kpurple <2kpurple@gmail.com>
2016-12-27 01:18:19 +0800

    First commit.

---
+++ Currency/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Currency
+//
+//  Created by PurpleK Fung on 2016/12/27.
+//  Copyright © 2016年 PurpleK Fung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
